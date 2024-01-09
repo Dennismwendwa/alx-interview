29a2eb1beb4854d1492c9de30152da51cb982967
Dennis Musembi Mwendwa <dennismusembi2@gmail.com>
2024-01-09 14:04:24 +0300

    searching for open boxes

---
+++ 0x01-lockboxes/0-lockboxes.py
@@ -0,0 +1,19 @@
+"""This method uses stack to keep track of open boxes"""
+
+
+def canUnlockAll(boxes):
+    """This method checks if all boxes can be opened"""
+    box_status = [False] * len(boxes)
+    box_status[0] = True
+
+    stack = [0]
+
+    while stack:
+        current_box = stack.pop()
+
+        for key in boxes[current_box]:
+            if key < len(boxes) and not box_status[key]:
+                box_status[key] = True
+                stack.append(key)
+
+    return all(box_status)
---
+++ 0x01-lockboxes/README.md
@@ -0,0 +1,1 @@
+Finding the key of all boxes
