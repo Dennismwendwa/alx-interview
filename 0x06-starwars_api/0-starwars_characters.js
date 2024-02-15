37f67123b8bfca653a46f3d7f3615bc17f50ccd4
Dennis Musembi Mwendwa <dennismusembi2@gmail.com>
2024-02-15 22:47:22 +0300

    star wars api

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,40 +1,26 @@
 #!/usr/bin/node
 // get movies
+
 const request = require('request');
+const url = 'https://swapi-api.hbtn.io/api/films/' + process.argv[2];
 
+request.get(url, function (err, response, body) {
+  if (!err) {
+    const urlchars = JSON.parse(body).characters;
+    const len = urlchars.length;
+    Names(0, urlchars[0], urlchars, len);
+  }
+});
 
-function getMovieCharacters(movieId) {
-  const swapiBaseUrl = 'https://swapi.dev/api/films/';
-
-  request.get(`${swapiBaseUrl}${movieId}/`, (error, response, body) => {
-    if (error) {
-      console.error('Error:', error);
-    } else if (response.statusCode === 200) {
-     const filmData = JSON.parse(body);
-     const characterUrls = filmData.characters;
-
-     charactersUrls.forEach((characterUrl) => {
-       request.get(characterUrl, (charError, charResponse, charBody) => {
-         if (charError) {
-           console.error('Error fetching character:', charError);
-         } else if (charResponse.statusCode === 200) {
-           const characterData = JSON.parse(charBody);
-           console.log(characterData.name);
-         } else {
-           console.error(`Error fetching character. Status code: ${charResponse.statusCode}`);
-         }
-       });
-     });
-    } else {
-      console.error(`Error: Unable to Movie ID ${movieId}. Status code: ${response.statusCode}`);
+function Names (i, url, chars, len) {
+  if (i === len) {
+    return;
+  }
+  request.get(url, function (err, response, body) {
+    if (!err) {
+      console.log(JSON.parse(body).name);
+      i++;
+      Names(i, chars[i], chars, len);
     }
   });
 }
-
-if (process.argv.length !== 3) {
-  console.log('Usage: node script.js <Movie ID>');
-}
-
-const movieId = process.argv[2];
-
-getMovieCharacters(movieId);
