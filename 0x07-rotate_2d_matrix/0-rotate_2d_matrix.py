4e38dbe49d87f892e28e57a2c64cf1c47de2c402
Dennis Musembi Mwendwa <dennismusembi2@gmail.com>
2024-02-20 08:13:25 +0300

    rotating 2D matrix

---
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""This script rotate matrix"""
+
+
+def rotate_2d_matrix(matrix):
+    """This function reverses the matrix"""
+    n = len(matrix)
+
+    # transpose the matrix
+    for k in range(n):
+        for m in range(k + 1, n):
+            matrix[k][m], matrix[m][k] = matrix[m][k], matrix[k][m]
+
+    # reverse each row
+    for k in range(n):
+        matrix[k].reverse()
