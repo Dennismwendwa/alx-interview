2457f56e3ff863cf5c362133bd321265c6b07692
Dennis Musembi Mwendwa <dennismusembi2@gmail.com>
2024-02-28 07:19:52 +0300

    Greedy Algorithms

---
+++ 0x08-making_change/0-making_change.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""Analyzing the time and space complexity of algorithms"""
+
+
+def makeChange(coins, total):
+    """
+    greedy algorithms and scenarios where they might not
+    provide the optimal solution
+    """
+    if total <= 0:
+        return 0
+    coins.sort(reverse=True)
+    coin_count = []
+
+    for coin in coins:
+        count = total // coin
+
+        coin_count.append(count)
+        total = total % coin
+        if total <= 0:
+            break
+
+    if total > 0:
+        return -1
+
+    return sum(coin_count)
+
+# int(makeChange([1, 2, 25], 37))
