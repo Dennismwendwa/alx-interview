3dcf80f0d1540fa3540c31bf137bd7591dfa6617
Dennis Musembi Mwendwa <dennismusembi2@gmail.com>
2024-03-12 21:24:52 +0300

    corrected bugs in the prime function

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -3,61 +3,30 @@
 
 
 def isWinner(x, nums):
-    """This is the gamfunction"""
-    def is_prime(num):
-        """checks if number is prime number"""
-        if num < 2:
-            return False
-        for i in range(2, int(num**0.5) + 1):
-            if num % i == 0:
-                return False
-        return True
-
-    def get_primes_up_to_n(n):
-        """Generates all primes"""
-        primes = []
-        for i in range(2, n + 1):
-            if is_prime(i):
-                primes.append(i)
-        return primes
-
-    def optimal_move_left(nums):
-        """For player moves"""
-        for prime in primes:
-            if any(num % prime == 0 for num in nums):
-                return True
-        return False
-
-    maria_wins = 0
-    ben_wins = 0
-
-    for n in nums:
-        """Game loop"""
-        primes = get_primes_up_to_n(n)
-        current_player = 'Maria'
-
-        while optimal_move_left(nums):
-            if current_player == 'Maria':
-                for prime in primes:
-                    if any(num % prime == 0 for num in nums):
-                        nums = [num for num in nums if num % prime != 0]
-                        current_player = 'Ben'
-                        break
-            else:
-                for prime in primes:
-                    if any(num % prime == 0 for num in nums):
-                        nums = [num for num in nums if num % prime != 0]
-                        current_player = 'Maria'
-                        break
-
-        if current_player == 'Maria':
-            ben_wins += 1
-        else:
-            maria_wins += 1
-
-    if maria_wins > ben_wins:
-        return 'Maria'
-    elif ben_wins > maria_wins:
-        return 'Ben'
-    else:
+    """Finding the winner of this game"""
+    if x < 1 or not nums:
         return None
+
+    marias_wins = 0
+    bens_wins = 0
+
+    n = max(nums)
+    primes = [True for h in range(1, n + 1, 1)]
+    primes[0] = False
+    for k, is_prime in enumerate(primes, 1):
+        if k == 1 or not is_prime:
+            continue
+        for s in range(k + k, n + 1, k):
+            primes[s - 1] = False
+
+    for p, n in zip(range(x), nums):
+        primes_count = len(list(filter(lambda x: x, primes[0: n])))
+        bens_wins += primes_count % 2 == 0
+        marias_wins += primes_count % 2 == 1
+
+    if marias_wins == bens_wins:
+        return None
+    elif marias_wins > bens_wins:
+        return 'Maria'
+    elif marias_wins < bens_wins:
+        return 'Ben'
